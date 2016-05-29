a9af1ab4ce64df84d765a085e6b53dc9e4874e26
somzzz <lucia_coj@yahoo.com>
2016-05-29 09:04:31 -0700

    dlopen dlclose dlsym for c sample

--- c_sample/sample.c
+++ c_sample/sample.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <dlfcn.h>
 
 int main(void) {
+	int rc;
+	void *handle;
+	char *error;
+	void  (*foo_function)(void);
 
 	printf("Hello world!");
-	int rc = chmod("/etc/passwd", 0444);
+	rc = chmod("/etc/passwd", 0444);
 	printf("%d", rc);
+
+	handle = dlopen("/usr/lib/libfoo.so", RTLD_LAZY);
+	if (!rc)
+		return -1;
+      
+    foo_function = dlsym(handle, "foo");
+    if ((error = dlerror()) != NULL)
+    	return -1;
+
+    (*foo_function)();
+
+    dlclose(handle);
 	
 	return 0;
 }
